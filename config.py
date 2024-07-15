3bf85ff8710306afa38c73df26884dcc983cf191
DelineaLaari <141368837+DelineaLaari@users.noreply.github.com>
2024-07-15 12:16:43 -0700

    Create config.py
    initial commit.

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+CERT_DIR = "certificates"
+PRIVATE_KEY = "private.key"
+CSR = "request.csr"
+CERTIFICATE = "certificate.crt"
+P12_CERTIFICATE = "certificate.p12"
+P12_PASSWORD = "yourstrongpassword"
+DAYS_VALID = 365
+ALT_NAMES = "DNS:example.com, DNS:www.example.com"
+SUBJECT_NAME = "/C=US/ST=State/L=City/O=Organization/OU=OrganizationalUnit/CN=your_common_name"
